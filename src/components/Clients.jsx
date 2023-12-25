e6468a01fcf97304989a84940161e9dc45c2303a
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-25 21:47:17 +0100

    feat: Create & style clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import styles, { layout } from "../style";
+import {clients} from '../constants';
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+    <section id='clients' className={`${styles.flexCenter} my-4`}>
+        <div className={`${styles.flexCenter} flex-wrap w-full`}>
+            {
+                clients.map(client => (
+                    <div key={client.id} className={`flex-1 flex-wrap ${styles.flexCenter} min-w-[120px] sm:min-w-[192px]`}>
+                        <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain hover:color-white client-logo" />
+                    </div>
+                ))
+            }
+        </div>
+    </section>
+);
 
-export default Clients
+export default Clients;
