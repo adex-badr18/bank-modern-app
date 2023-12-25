20811ddf3ef63b8178b0882d845f7fe435f53854
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-25 12:07:56 +0100

    feat: Create & style hero section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import styles from "../style";
+import { arrowUp } from "../assets";
 
 const GetStarted = () => {
   return (
-    <div>GetStarted</div>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer sm:hover:scale-105 transition ease-in-out delay-150`}>
+        <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+            <div className={`${styles.flexStart} flex-row`}>
+                <p className="font-poppins font-medium text-[18px] leading-[23px] text-gradient mr-2">Get</p>
+                <img src={arrowUp} className="w-[23px] h-[23px] object-contain" alt="arrow" />
+            </div>
+
+            <p className="font-poppins font-medium text-[18px] leading-[23px] text-gradient">Started</p>
+        </div>
+    </div>
   )
 }
 
