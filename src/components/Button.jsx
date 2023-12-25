742aa1f1dd317aaaab3a8512a3eb5b5dad737fcd
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-25 23:12:16 +0100

    feat: Create & style footer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ styles }) => {
     return (
-        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] ${styles}`}>
+        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] sm:hover:scale-105 transition ease-in-out delay-150 ${styles}`}>
             Get Started
         </button>
     )
